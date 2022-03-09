99dc7851f02b5dcefe41cf679be169e4abdfddab
t-sin <shinichi.tanaka45@gmail.com>
2022-03-09 23:44:58 +0900

    Add a macro to define built-in words

--- builtin.s
+++ builtin.s
@@ -14,6 +14,43 @@
 
 .include "macro.s"
 
+# define words and create its dictionary entry
+#
+# input:
+#   name:    name string with a length `namelen`
+#   namelen: length of `name`. it must be less than 7
+#   flags:   if non-zero this word is an immediate word.
+#            so, in compilation mode, this word is executed, not compiled.
+#
+	.set _defword_link, 0
+
+	.macro DEFWORD name namelen flags
+
+	.ifge \namelen - 8
+	.error "namelen is too large: \namelen"
+	.endif
+
+	.set _defword_head, .
+
+	.globl word_\name
+	.section .text
+	# name field
+	.align 8
+	.byte \namelen + \flags
+	.ascii "\name"
+
+	# link field
+	.align 8
+	.dc.a _defword_link
+	.set _defword_link, _defword_head
+
+	# codeptr
+	.align 8
+	.dc.a . + 8
+
+word_\name:
+	.endm
+
 ##
 # built-in words
 #
@@ -21,10 +58,10 @@
 
 ## Forth primitives
 
-# this is a starter words for user-defined words.
-# the code of user-defined words must start with DOCOL words.
+# this is a starter word for colon-defined words.
+# the code of colon-defined words must start with DOCOL words.
 #
-word_docol:
+	DEFWORD "docol", 5, 0
 	mov rax, [r15]
 	add r15, 8
 	mov rbx, r15
