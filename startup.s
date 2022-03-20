b1060c12de8e3fd414464c96c7ca33eb1afbf63b
t-sin <shinichi.tanaka45@gmail.com>
2022-03-20 22:51:01 +0900

    Add LIT macro

--- startup.s
+++ startup.s
@@ -7,40 +7,39 @@
 ##
 # startup codes
 
+	.macro LIT val
+	.dc.a word_LIT
+	.dc.a \val
+	.endm
+
 	.macro DOCOL codeptr
 	.dc.a word_DOCOL
 	.dc.a \codeptr
 	.endm
 
 	.macro PUTCH ch
-	.dc.a word_LIT
-	.dc.a \ch
+	LIT \ch
 	.dc.a word_EMIT
 	.endm
 
 main_code:
 	# create a word
-	.dc.a word_LIT
-	.dc.a 0
+	LIT 0
 	DOCOL prompt_name_code
-	.dc.a word_LIT
-	.dc.a '\n
+	LIT '\n
 	.dc.a word_PARSE
 	.dc.a word_CREATE
 
 	# create a word pt.2
-	.dc.a word_LIT
-	.dc.a 0
+	LIT 0
 	DOCOL prompt_name_code
-	.dc.a word_LIT
-	.dc.a '\n
+	LIT '\n
 	.dc.a word_PARSE
 	.dc.a word_CREATE
 
 	# find word created above
 	DOCOL prompt_name_code
-	.dc.a word_LIT
-	.dc.a '\n
+	LIT '\n
 	.dc.a word_PARSE
 	.dc.a word_FIND
 	.dc.a word_DROP
