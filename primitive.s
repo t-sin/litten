4cdcf76c7a7f78b886962a2a188b96c14571d3c5
t-sin <shinichi.tanaka45@gmail.com>
2022-03-24 13:43:02 +0900

    Revert "Remove macro.s: macros in macro.s are only used in primitive.s"
    macro.s also used in syscall.s

--- primitive.s
+++ primitive.s
@@ -1,44 +1,7 @@
 	.intel_syntax noprefix
 	.text
 
-##
-# macros for primitive operations
-#
-
-# progress IP with built-in words
-	.macro NEXT
-	jmp inner_interpreter
-	.endm
-
-##
-# parameter stack operation
-
-# push a cell to the pstack
-	.macro PPUSH reg
-	sub r13, 8
-	mov qword ptr [r13], \reg
-	.endm
-
-# pop a cell from the pstack
-	.macro PPOP reg
-	mov \reg, qword ptr [r13]
-	add r13, 8
-	.endm
-
-##
-# return stack operation
-
-# push a cell to the return stack
-	.macro RPUSH reg
-	sub r14, 8
-	mov qword ptr [r14], \reg
-	.endm
-
-# pop a cell from the return stack
-	.macro RPOP reg
-	mov \reg, qword ptr [r14]
-	add r14, 8
-	.endm
+.include "macro.s"
 
 ##
 # dictionary entry
