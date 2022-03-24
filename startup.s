71ca654d6298cc3bb6a73458d874554f00cd9bff
t-sin <shinichi.tanaka45@gmail.com>
2022-03-24 10:57:25 +0900

    Rewrite startup codes and macros for it

--- startup.s
+++ startup.s
@@ -5,193 +5,226 @@
 	.data
 
 ##
-# convinient macros to define code
+# macros for compiling code
 #
 
+# embed a primitive named `label` to execute immediately.
 	.macro PRIMITIVE label
 	.dc.a primitive_\label
 	.endm
 
-	.macro LIT val
+# embed a value as a literal to push immediately.
+	.macro LITERAL val
 	PRIMITIVE LIT
 	.dc.a \val
 	.endm
 
+# embed an invocation of data field address to execute immediately.
+# this is used only the codeptr is staticaly determined.
 	.macro DOCOL codeptr
 	PRIMITIVE DOCOL
 	.dc.a \codeptr
 	.endm
 
-	.macro CMP_L val
-	LIT \val
+# embed codes to compile a valueas a literal into HERE.
+	.macro COMPILE_LIT val
+	LITERAL primitive_LIT
+	PRIMITIVE COMMA
+	LITERAL \val
 	PRIMITIVE COMMA
 	.endm
 
-	.macro CMP_P label
-	LIT primitive_\label
+# embed codes to compile a word invocation into HERE
+	.macro COMPILE_PRIM label
+	LITERAL primitive_\label
 	PRIMITIVE COMMA
 	.endm
 
-	.macro PUTCH ch
-	LIT \ch
-	PRIMITIVE EMIT
-	.endm
+##
+# word-defining macros
+#
 
-# define a code defining word named by `name`
+# define a code defining word named by `name`.
 # when DOCOL a label create by this macro `defword_\label,
 # CREATE and COMPILE a word behaved as the code between DEFWORD and ENDDEF at run-time.
-#
 	.macro DEFWORD name label flags
 	.align 8
 word_\label:
 	.ascii "\name"
 	.set word_\label\()_namelen, . - word_\label
 	.align 8
-defword_\label:
-	LIT \flags
-	LIT word_\label
-	LIT word_\label\()_namelen
+define_word_\label:
+	LITERAL \flags
+	LITERAL word_\label
+	LITERAL word_\label\()_namelen
 	PRIMITIVE CREATE
 	.endm
 
+# close a code definition.
 	.macro ENDDEF
 	PRIMITIVE EXIT
 	.endm
 
+# invoke a word definition.
+# after this macro, a word named `label` is added in the dictionary.
 	.macro DEFINE label
-	DOCOL defword_\label
+	DOCOL define_word_\label
 	.endm
 
+# invoke a word defined at run-time.
 	.macro EXECUTE label
-	LIT word_\label
-	LIT word_\label\()_namelen
+	LITERAL word_\label
+	LITERAL word_\label\()_namelen
 	PRIMITIVE FIND
 	PRIMITIVE DROP
 	PRIMITIVE TO_BODY
 	PRIMITIVE EXEC
 	.endm
-
-	.macro CMP_E label
-	CMP_L word_\label
-	CMP_L word_\label\()_namelen
-	CMP_P FIND
-	CMP_P DROP
-	CMP_P TO_BODY
-	CMP_P EXEC
-	.endm
-
-##
-# word-defining macro example
 #
-#	DEFWORD "FOUR", "FOUR", 0
-#	CMP_P LIT
-#	CMP_L 4
-#	CMP_P EXIT
-#	ENDDEF
-#
-# initialize:
-#	DEFINE FOUR
-#	PRIMITIVE EXIT
-#
-# main_code:
-#	DOCOL initialize
-#	EXECUTE FOUR
-#	PRIMITIVE EMIT
-#	PRIMITIVE QUIT
+#	.macro COMPILE_WORD label
+#	LITERAL word_\label
+#	LITERAL word_\label\()_namelen
+#	PRIMITIVE FIND
+#	PRIMITIVE DROP
+#	PRIMITIVE TO_BODY
+#	COMPILE_PRIM EXEC
+#	PRIMITIVE COMMA
+#	.endm
 #
 
-# redefine primitives as words
-#
-	.macro REDEF name label flags
-	DEFWORD "\name", "\label", \flags
-	CMP_P \label
-	ENDDEF
-	.endm
+# defining macro example:
 
-# export a pirimitive by setting up
-# to hide dengerous primitives in the primitive dictionary
-#
-	.macro EXPORT label
-	DOCOL defword_\label
-	.endm
-
-	REDEF "EMIT", "EMIT", 0x00
-	REDEF "=", "EQ", 0x00
-
-export_primitives:
-	EXPORT EMIT
-	EXPORT EQ
-	PRIMITIVE EXIT
-
-##
-# built-in words
-#
-
-# start to branch conditionally. this word is available in compiler mode.
-#
-# C: ( bool -- )
-#
-	DEFWORD "IF", "IF", 0x80
-	CMP_P BZ
-	PRIMITIVE FW_MARK
+	DEFWORD "FOUR", "FOUR", 0
+	COMPILE_LIT '*
+	COMPILE_PRIM EMIT
+	COMPILE_PRIM EXIT
 	ENDDEF
 
-# introduce an else clause to IF. this word is available in compiler mode.
-#
-# C: ( addr -- addr )
-#
-	DEFWORD "ELSE", "ELSE", 0x80
-	PRIMITIVE FW_MARK
-	PRIMITIVE FW_RESOLVE
-	ENDDEF
-
-# terminate IF with catch branching. this word is available in compiler mode.
-#
-# C: ( addr -- )
-#
-	DEFWORD "THEN", "THEN", 0x80
-	PRIMITIVE FW_RESOLVE
-	ENDDEF
-
-# a word for testing IF ~ ELSE ~ THEN
-# ( u -- )
-	DEFWORD "EMITZNZ", "EMITZNZ", 0x00
-	EXECUTE IF
-	CMP_L 'z
-	CMP_E EMIT
-	EXECUTE ELSE
-	CMP_L 'n
-	CMP_E EMIT
-	CMP_L 'z
-	CMP_E EMIT
-	EXECUTE THEN
-	CMP_P EXIT
-	ENDDEF
-
-# read one token delimited with `char`
-#
-# ( char -- addr )
-#
-#   char: a delimiter character
-#   addr: a pointer to string read formatted as bytes: [len ch0 ch1 ...]
-#
-	DEFWORD "WORD", "WORD", 0x00
-	# IFを実装してから書く
-	ENDDEF
-
-##
-# startup codes
-
-setup_builtins:
-#	DOCOL defword_WORD
-	DEFINE IF
-	DEFINE ELSE
-	DEFINE THEN
+initialize:
+	DEFINE FOUR
 	PRIMITIVE EXIT
 
 main_code:
-	DOCOL export_primitives
-	DOCOL setup_builtins
-	DEFINE EMITZNZ
-	EXECUTE EMITZNZ
+	DOCOL initialize
+	EXECUTE FOUR
 	PRIMITIVE QUIT
+
+## REWRITEING...
+#
+#
+## redefine primitives as words
+##
+#	.macro REDEF name label flags
+#	DEFWORD "\name", "\label", \flags
+#	CMP_P \label
+#	ENDDEF
+#	.endm
+#
+## export a pirimitive by setting up
+## to hide dengerous primitives in the primitive dictionary
+##
+#	.macro EXPORT label
+#	DOCOL defword_\label
+#	.endm
+#
+#	REDEF "EMIT", "EMIT", 0x00
+#	REDEF ">R", "RPUSH", 0x00
+#	REDEF ">R", "RPOP", 0x00
+#	REDEF "=", "EQ", 0x00
+#	REDEF "NOT", "NOT", 0x00
+#
+#export_primitives:
+#	EXPORT EMIT
+#	EXPORT RPUSH
+#	EXPORT RPOP
+#	EXPORT EQ
+#	EXPORT NOT
+#	PRIMITIVE EXIT
+#
+###
+## built-in words
+##
+#
+## start to branch conditionally. this word is available in compiler mode.
+##
+##    ( bool -- bool )
+##
+## in compilation:
+##     ( -- addr )
+##
+#	DEFWORD "IF", "IF", 0x80
+#	LIT_P DUP
+#	CMP_P COMMA
+#	LIT_P BZ
+#	CMP_P COMMA
+#	CMP_P FW_MARK
+#	ENDDEF
+#
+## introduce an else clause to IF. this word is available in compiler mode.
+##
+##    ( bool -- )
+##
+## in compilation:
+##    ( addr -- addr )
+##
+#	DEFWORD "ELSE", "ELSE", 0x80
+#	CMP_P FW_RESOLVE
+#	LIT_P  NOT
+#	CMP_P COMMA
+#	LIT_P  NOT
+#	CMP_P COMMA
+#	CMP_P FW_MARK
+#	ENDDEF
+#
+## terminate IF with catch branching. this word is available in compiler mode.
+##
+## C: ( addr -- )
+##
+#	DEFWORD "THEN", "THEN", 0x80
+#	CMP_P FW_RESOLVE
+#	ENDDEF
+#
+## a word for testing IF ~ ELSE ~ THEN
+## ( u -- )
+#	DEFWORD "EMITZNZ", "EMITZNZ", 0x00
+#	EXECUTE IF
+#	CMP_L 'z
+#	CMP_W EMIT
+#	EXECUTE ELSE
+#	CMP_L 'n
+#	CMP_W EMIT
+#	CMP_L 'z
+#	CMP_W EMIT
+#	EXECUTE THEN
+#	CMP_P EXIT
+#	ENDDEF
+#
+## read one token delimited with `char`
+##
+## ( char -- addr )
+##
+##   char: a delimiter character
+##   addr: a pointer to string read formatted as bytes: [len ch0 ch1 ...]
+##
+#	DEFWORD "WORD", "WORD", 0x00
+#	# IFを実装してから書く
+#	ENDDEF
+#
+###
+## startup codes
+#
+#setup_builtins:
+##	DOCOL defword_WORD
+#	DEFINE IF
+#	DEFINE ELSE
+#	DEFINE THEN
+#	PRIMITIVE EXIT
+#
+#main_code:
+#	DOCOL export_primitives
+#	DOCOL setup_builtins
+#	DEFINE EMITZNZ
+#	LITERAL 1
+#	EXECUTE EMITZNZ
+#	PRIMITIVE QUIT
+
